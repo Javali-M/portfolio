34e4fb0e6d92aa5e372f05c8a1ac82fdd9fceea8
Javali <javalimurari@gmail.com>
2022-08-29 23:48:51 +0530

    added tech.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,28 +1,33 @@
 import React from 'react'
 import './Hero.css';
 import img from '../assets/dk.jpeg';
-import me from '../assets/gp.png';
+import me from '../assets/j.png';
 const Hero = () => {
   return (
-    <div className='hero'>
-        <div className='mask'>
-            <img src={img} className='img' ></img>
+    <div className="hero">
+      <div className="mask">
+        <img src={img} className="img"></img>
+      </div>
+      <div className="content">
+        <div className="first">
+          <p> Hey there! I'm</p>
+          <br/>
+          <p className="name" >Javali Murari.</p>
+          <br />
+          <br />
+          <p>
+            I am a full-stack web developer based in India with a passion for
+            building and designing websites. I am enthusiastic about user
+            experiences and love to know about their journeys. Currently also
+            working on Machine Learning projects. I’m a team player with an eye
+            for detail. When not on my laptop, I listen to music, sketch and
+            binge-watch stuff !
+          </p>
         </div>
-        <div className='content'>
-        
-            <p> Hey there! I'm</p>
-            <p className='name'>Javali Murari.</p>
-            {/* <div className='me'>
-            <img src={me}  ></img> 
-            </div> */}
-           
-            <br/>
-            {/* <h1>A full stack developer.</h1> */}
-            <br/>
-            <p>I am a full-stack web developer based in India with a passion for building and designing websites. 
-I am enthusiastic about user experiences and love to know about their journeys. Currently also working on Machine Learning projects. I’m a team player with an eye for detail. 
-When not on my laptop, I listen to music, sketch and binge-watch stuff !</p>
+        <div className='me'>
+        <img className="me" src={me}></img>
         </div>
+      </div>
     </div>
   );
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Footer from '../components/Footer';
+import Icons from '../components/Icons';
+import Tech from '../components/Tech';
 const Home = () => {
   return (
     <div>
     
     <Navbar/>
     <Hero/>
+    <Tech/>
     <Footer/>
     </div>
   );
