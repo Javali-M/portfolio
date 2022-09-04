5f98154331ba820896aa5bfeb3d6fb54ec8b6e97
Javali <javalimurari@gmail.com>
2022-09-04 23:24:59 +0530

    updated Contact

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Navbar } from 'react-bootstrap'
+import Footer from '../components/Footer'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>Contact
+     <Navbar/>
+    <h1>Contact me</h1>
+    <Footer/>
+    </div>
   )
 }
 
