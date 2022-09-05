dde6e7152165e728bf3af07211a3489582e46501
Javali <javalimurari@gmail.com>
2022-09-05 23:13:44 +0530

    created Form

---
+++ src/components/Hero2.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Hero2.css'
+export const Hero2 = () => {
+  return (
+    <div>Hero2</div>
+  )
+}
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Navbar } from 'react-bootstrap'
 import Footer from '../components/Footer'
+import Form from '../components/Form'
+import img from '../assets/dk.jpeg';
 
 const Contact = () => {
   return (
-    <div>Contact
+    <div>
      <Navbar/>
-    <h1>Contact me</h1>
+     <div className="mask">
+        <img src={img} className="img" heading="contact me"></img>
+      </div>
+      <Form/>
     <Footer/>
     </div>
   )
